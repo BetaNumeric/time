dfb182bc6357e92ad2dcbe41cfa7f694dae86e3c
Tim <101401062+BetaNumeric@users.noreply.github.com>
2022-03-13 02:47:04 +0100

    Update time_web.js

--- time_web.js
+++ time_web.js
@@ -53,7 +53,7 @@ var imgMag=new Array(tabNum);
 var imgDragStart=0;
 var record=false;
 var stopTime=true;
-var showSeconds=false;
+var showSeconds=true;
 var mouseWasDragged=false;
 
 var sketchWidth=1280, sketchHeight=720;
@@ -1566,7 +1566,7 @@ function keyPressed() {
   if (key==='r') {
     record=!record;
   }
-  if (key==="s" || key==="S") {
+  if (key==="s") {
     showSeconds=!showSeconds;
   }
   if (keyCode===ENTER) {
